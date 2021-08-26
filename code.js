0b6dce12d827f89361611c816497b3769eca8f62
dayvedallen <dayvedallen@gmail.com>
2021-08-26 11:55:38 -0400

    lost

--- code.js
+++ code.js
@@ -16,7 +16,37 @@ function fetechCity(coords) {
   //  fetch(`https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lon}&key=${key}`)
     fetch(request)
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+        let components = data.results[0].components
+        console.log(components)
+        let townOrCityOrVillage = ""
+        if(components.hasOwnProperty("city") === true) {
+            townOrCityOrVillage = components.city
+        } else if(components.town) {
+            townOrCityOrVillage = componens.town 
+        } else if(components.village) {
+            townOrCityOrVillage = components.village
+        }
+        let locationDiv = document.getElementById("location")
+        locationDiv.innerText = `${townOrCityOrVillage}. ${components.country}, ${components.continent}`
+        console.log(townOrCityOrVillage)
+    });
+}
+
+function fetchWeather(location) {
+    fetch(`https://goweather.herokuapp.com/weather/${location}`)
+    .then((response) => response.json())
+    .then((data => {
+        let today = [data.temperature, data.wind, data.description]
+        console.log(today)
+        displayWeather(today, data.forecast)
+    })
+}
+
+function displayWeather(todaysWeather, mextThreeDays) {
+    let todayDiv = document.getElementById("today")
+    let threeDaysDiv = document.getElementById("threeDays")
+    todayDiv.innerText = `Today, the temperature is ${todaysWeather[0]} with ${} winds and s{} kind of weather.`
 }
 
 navigator.geolocation.getCurrentPosition(
